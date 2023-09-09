093aec699182a50368d9e58014cba660fa259d2d
Hrvoje Lesar <hrvoje.lesar1@hotmail.com>
2023-09-09 20:02:17 +0200

    Rate limiter

---
+++ src/EventSubscriber/RequestRateLimitSubscriber.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\RateLimiter\RateLimiterFactory;
+
+class RequestRateLimitSubscriber implements EventSubscriberInterface
+{
+    public function __construct(protected RateLimiterFactory $authenticatedApiLimiter)
+    {
+    }
+
+    /**
+     * @return void
+     * @throws TooManyRequestsHttpException
+     */
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $limiter = $this->authenticatedApiLimiter->create($event->getRequest()->getClientIp());
+        if ($limiter->consume()->isAccepted() === false) {
+            throw new TooManyRequestsHttpException();
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::REQUEST => 'onKernelRequest',
+        ];
+    }
+}
